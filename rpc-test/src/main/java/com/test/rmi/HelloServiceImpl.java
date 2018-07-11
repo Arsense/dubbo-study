787e2cc8785341789ff2ccecaf0254bf08142633
Arsense <466217395@qq.com>
2018-07-11 18:42:08 +0800

    add RMI test

--- rpc-test/src/main/java/com/test/rmi/HelloServiceImpl.java
+++ rpc-test/src/main/java/com/test/rmi/HelloServiceImpl.java
@@ -1,13 +1,23 @@
 package com.test.rmi;
 
 import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
-/**
+/** RMI test
  * @author tangwei
  * @date 2018/7/11 18:26
  */
-public class HelloServiceImpl implements HelloService{
+public class HelloServiceImpl  extends UnicastRemoteObject implements HelloService{
+
+
     public String sayHello(String guest) throws RemoteException {
-        return null;
+        return "hello,"+guest;
     }
+
+    public  HelloServiceImpl() throws  RemoteException{
+        super();
+    }
+
+
+
 }
---
+++ rpc-test/src/main/java/com/test/rmi/Server.java
@@ -0,0 +1,26 @@
+package com.test.rmi;
+
+import java.net.MalformedURLException;
+import java.rmi.AlreadyBoundException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+
+/** 原因在于需要继承UnicastRemoteObject
+ * @author tangwei
+ * @date 2018/7/11 18:35
+ */
+public class Server {
+
+
+    public static void main(String[] args) throws RemoteException, AlreadyBoundException, MalformedURLException {
+        //创建注册服务
+
+        HelloService helloService = new HelloServiceImpl();
+        LocateRegistry.createRegistry(8801);
+
+        Naming.bind("rmi://localhost:8801/helloService",helloService);
+        System.out.println("Server provide RPC serivce now");
+
+    }
+}
