e1e0ae9fb052177ee71d50112d406ff22ed9f28b
Arsense <466217395@qq.com>
2018-07-31 16:03:24 +0800

    shrowd config

--- rpc-shrowd/src/main/java/city/hunter/begin/ShrowdBeanDefinationParser.java
+++ rpc-shrowd/src/main/java/city/hunter/begin/ShrowdBeanDefinationParser.java
@@ -1,17 +1,19 @@
 package city.hunter.begin;
 
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.ManagedMap;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.beans.factory.xml.BeanDefinitionParser;
 import org.springframework.beans.factory.xml.ParserContext;
 import org.w3c.dom.Element;
+import java.util.HashSet;
+import java.util.Set;
 
 /**  这里对应解析装载 xml的配置类 并加入到相应的
  * @author tangwei
  * @date 2018/7/30 10:49
  */
 public class ShrowdBeanDefinationParser implements BeanDefinitionParser {
-
 
     private final Class<?> beanClass;
 
@@ -24,9 +26,42 @@ public class ShrowdBeanDefinationParser implements BeanDefinitionParser {
 
         //用来加载我们的配置类
         RootBeanDefinition beanDefinition = new RootBeanDefinition();
-//        beanDefinition.
+        //解析相应的Bean 然后装载到Spring
+
+        //先判断是否有ID 没有就自己设定
+        String id = element.getAttribute("id");
+
+        if (id == null ||id.length() == 0) {
+            String initBeanName = element.getAttribute("name");
+            if (initBeanName == null ||initBeanName.length() == 0) {
+               // initBeanName = element.getAttribute("interface");
+                //没有name 就用类名做beanName
+                initBeanName = beanClass.getName();
+            }
+            id = initBeanName;
+        }
+        if (id != null && id.length() >0) {
+            //获得属性的键值
+            beanDefinition.getPropertyValues().addPropertyValue("id",id);
+        }
+
+        getBeanClassSetterAndGetter(beanDefinition,beanClass);
 
         return beanDefinition;
+    }
+
+    /**
+     *
+     * @param beanDefinition
+     * @param beanClass
+     */
+    private void getBeanClassSetterAndGetter(RootBeanDefinition beanDefinition,Class<?> beanClass) {
+
+        Set<String> props = new HashSet<String>();
+        ManagedMap parameters = null;
+        //这里是获取类的getter 和setter方法
+//        for ()
+
     }
 
 
