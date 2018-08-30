6fd7be16c59be3dee7d0b37e6bb9eb0fb48351e1
Arsense <466217395@qq.com>
2018-08-30 17:20:24 +0800

    自定义XML OK

--- remote-demo/src/main/java/com/remote/test/Server.java
+++ remote-demo/src/main/java/com/remote/test/Server.java
@@ -3,9 +3,6 @@ package com.remote.test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import java.net.MalformedURLException;
-import java.rmi.AlreadyBoundException;
-import java.rmi.RemoteException;
 
 
 /** 原因在于需要继承UnicastRemoteObject
@@ -17,7 +14,7 @@ public class Server {
 
     private final static Logger LOG =  LoggerFactory.getLogger(Server.class);
 
-    public static void main(String[] args) throws RemoteException, AlreadyBoundException, MalformedURLException {
+    public static void main(String[] args) {
         //创建注册服务
 
 //        HelloService helloService = new HelloServiceImpl();
---
+++ remote-demo/src/main/java/com/remote/test/TestService.java
@@ -0,0 +1,8 @@
+package com.remote.test;
+
+/**
+ * @author tangwei
+ * @date 2018/8/30 17:16
+ */
+public class TestService {
+}
