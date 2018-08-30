0c9a1ee90321f641c3c316cc73a12edc6e977fe9
Arsense <466217395@qq.com>
2018-08-30 18:09:04 +0800

    Netty基础添加

---
+++ remote-demo/src/main/java/com/remote/test/netty/NettyServer.java
@@ -0,0 +1,8 @@
+package com.remote.test.netty;
+
+/**
+ * @author tangwei
+ * @date 2018/8/30 18:08
+ */
+public class NettyServer {
+}
--- remote-demo/src/main/java/com/remote/test/service/ProviderFactoryBean.java
+++ remote-demo/src/main/java/com/remote/test/service/ProviderFactoryBean.java
@@ -1,16 +1,50 @@
 package com.remote.test.service;
 
-/**
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.InitializingBean;
+
+/** FactoryBean 工厂Bean定义
  * @author tangwei
  * @date 2018/8/30 17:38
  */
-public class ProviderFactoryBean {
+public class ProviderFactoryBean implements FactoryBean, InitializingBean {
+//    InitializingBean接口为bean提供了初始化方法的方式，它只包括afterPropertiesSet方法，凡是继承该接口的类，在初始化bean的时候会执行该方法
 
     //服务接口
     private Class<?> serviceInterface;
 
+    //服务实现
+    private Object serviceObject;
+    //服务端口
     private String port;
 
+
+
+    public void afterPropertiesSet() throws Exception {
+            //这里启动Netty
+    }
+
+
+    public Object getObject() throws Exception {
+        return serviceObject;
+    }
+
+
+    public boolean isSingleton() {
+        return true;
+    }
+
+    public Class<?> getObjectType() {
+        return serviceInterface;
+    }
+
+    public Object getServiceObject() {
+        return serviceObject;
+    }
+
+    public void setServiceObject(Object serviceObject) {
+        this.serviceObject = serviceObject;
+    }
 
     public Class<?> getServiceInterface() {
         return serviceInterface;
@@ -37,4 +71,7 @@ public class ProviderFactoryBean {
                 ", port='" + port + '\'' +
                 '}';
     }
+
+
+
 }
