e90f64c194ad775a64a26e4d8e4baa69de5f4c3f
Arsense <466217395@qq.com>
2018-08-30 17:26:04 +0800

    Bean读取优化一版

--- remote-demo/src/main/java/com/remote/test/service/ProviderBeanDefinitionParser.java
+++ remote-demo/src/main/java/com/remote/test/service/ProviderBeanDefinitionParser.java
@@ -1,16 +1,27 @@
 package com.remote.test.service;
 
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.xml.BeanDefinitionParser;
-import org.springframework.beans.factory.xml.ParserContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.xml.AbstractSingleBeanDefinitionParser;
 import org.w3c.dom.Element;
 
 /**
  * @author tangwei
  * @date 2018/8/30 17:09
  */
-public class ProviderBeanDefinitionParser  implements BeanDefinitionParser {
-    public BeanDefinition parse(Element element, ParserContext parserContext) {
-        return null;
+public class ProviderBeanDefinitionParser extends AbstractSingleBeanDefinitionParser {
+
+    //  日志输出
+    private static final Logger LOG = LoggerFactory.getLogger(ProviderBeanDefinitionParser.class);
+    @Override
+    protected void doParse(Element element, BeanDefinitionBuilder builder) {
+        //配置属性的读取
+
     }
+
+
+
+
+
 }
