19ccfd032a206659135b6b7e92b491d67bde92de
Arsense <466217395@qq.com>
2018-09-03 17:45:35 +0800

    删除无用的mini

--- rpc-mini/src/main/java/league/of/test/HelloSevice.java
+++
@@ -1,8 +0,0 @@
-package league.of.test;
-
-/**
- * @author tangwei
- * @date 2018/8/4 14:16
- */
-public interface HelloSevice {
-}
--- rpc-mini/src/main/java/league/of/top/SkillRequest.java
+++
@@ -1,8 +0,0 @@
-package league.of.top;
-
-/**
- * @author tangwei
- * @date 2018/8/4 19:20
- */
-public class SkillRequest {
-}
