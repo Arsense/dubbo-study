3006d271d28db916eda0ee0c2ac50040ef0f4c3e
Arsense <466217395@qq.com>
2018-11-27 15:29:02 +0800

    xml配置到Bean

--- dubbo-config/dubbo-config-spring/src/main/java/com/tw/dubbo/config/spring/DubboBeanDefinitionParser.java
+++ dubbo-config/dubbo-config-spring/src/main/java/com/tw/dubbo/config/spring/DubboBeanDefinitionParser.java
@@ -1,6 +1,7 @@
 package com.tw.dubbo.config.spring;
 
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.beans.factory.xml.BeanDefinitionParser;
 import org.springframework.beans.factory.xml.ParserContext;
 import org.w3c.dom.Element;
@@ -27,9 +28,45 @@ public class DubboBeanDefinitionParser implements BeanDefinitionParser {
         this.required = required;
     }
 
-
+    /**
+     * 解析加载xml配置到Spring 的Bean中
+     * @param element
+     * @param parserContext
+     * @return
+     */
     @Override
     public BeanDefinition parse(Element element, ParserContext parserContext) {
-        return null;
+        return parse(element, parserContext, beanClass, required);
+    }
+
+
+    private static BeanDefinition parse(Element element, ParserContext parserContext
+            , Class<?> beanClass, boolean required){
+        RootBeanDefinition beanDefinition = new RootBeanDefinition();
+        beanDefinition.setBeanClass(beanClass);
+        //设置懒加载 false就是立即加载beanDefinition
+        beanDefinition.setLazyInit(false);
+        //从xml取出 id=""后面的值
+        String id = element.getAttribute("id");
+        //没有Id 可能是<dubbo:protocol name="dubbo" port="20880"/>
+        //或者是  <dubbo:application name="demo-provider"/>
+        if ((id == null || id.length() == 0) && required) {
+            String generatedBeanName = element.getAttribute("name");
+            //name 也没有 基本可能是interface配置了
+            if (generatedBeanName == null || generatedBeanName.length() == 0) {
+
+            }
+            if (generatedBeanName == null || generatedBeanName.length() == 0) {
+                generatedBeanName = beanClass.getName();
+            }
+        }
+//      <bean id="demoService" class="org.apache.dubbo.demo.provider.DemoServiceImpl"/>
+
+        if (id != null && id.length() > 0) {
+
+        }
+
+
+            return null;
     }
 }
--- dubbo-config/dubbo-config-spring/src/main/java/com/tw/dubbo/config/spring/ServiceBean.java
+++ dubbo-config/dubbo-config-spring/src/main/java/com/tw/dubbo/config/spring/ServiceBean.java
@@ -1,9 +1,13 @@
 package com.tw.dubbo.config.spring;
 
+import com.tw.dubbo.config.ProviderConfig;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactoryUtils;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+
+import java.util.Map;
 
 /**
  * @author tangwei
@@ -14,14 +18,40 @@ public class ServiceBean<T> implements InitializingBean,ApplicationContextAware 
     //Spring上下背景文
     private static transient ApplicationContext SPRING_CONTEXT;
 
+    private transient ApplicationContext applicationContext;
 
+
+    private ProviderConfig provider;
+
+    /**
+     * Spring加载完后会启动 这里
+     * @throws Exception
+     */
     @Override
     public void afterPropertiesSet() throws Exception {
+        //对基本的配置做初始化 没有的就加载相应的配置
+        if (getProvider() == null) {
+            //从Spring上下背景文中取出相应的Config  从handlerMapping中取
+            Map<String, ProviderConfig> providerConfigMap = applicationContext == null ? null : BeanFactoryUtils.beansOfTypeIncludingAncestors(applicationContext, ProviderConfig.class, false, false);
+            if (providerConfigMap != null && providerConfigMap.size() > 0) {
 
-    }
+            } else {
+
+            }
+
+        }
+
+
+
+
+        }
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 
+    }
+
+    public ProviderConfig getProvider() {
+        return provider;
     }
 }
