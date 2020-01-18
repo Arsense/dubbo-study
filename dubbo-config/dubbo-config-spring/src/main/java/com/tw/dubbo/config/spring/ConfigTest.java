d532bd87ff5d7a7a0e2a1fb771ef04a5d5e609fb
Arsense <466217395@qq.com>
2020-01-19 02:31:38 +0800

    config test

---
+++ dubbo-config/dubbo-config-spring/src/main/java/com/tw/dubbo/config/spring/ConfigTest.java
@@ -0,0 +1,14 @@
+package com.tw.dubbo.config.spring;
+
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+//@Ignore
+public class ConfigTest {
+
+    @Test
+    public void testDemo(){
+        System.out.println("demo test");
+    }
+}
